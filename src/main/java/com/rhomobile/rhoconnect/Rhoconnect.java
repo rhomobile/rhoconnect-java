8deea2eed7721d1669af99f98ec251b91bc27cbe
Alexander Babichev <alexb@rhomobile.com>
2011-11-18 16:44:20 -0800

    Updated README.

--- src/main/java/com/rhomobile/rhoconnect/Rhoconnect.java
+++ src/main/java/com/rhomobile/rhoconnect/Rhoconnect.java
@@ -3,5 +3,5 @@ package com.rhomobile.rhoconnect;
 import java.util.Map;
 
 public interface Rhoconnect {
-    boolean authenticate(String login, String password, Map<String, Object> attribures);    
+    boolean authenticate(String login, String password, Map<String, Object> attributes);
 }
